c66d0a6b3b4b9879babd2b50ed641f33dd154022
Quentin ADAM <quentin.adam@waxzce.org>
2013-04-11 22:32:15 +0200

    suffix & prefix

--- public/js/site.js
+++ public/js/site.js
@@ -1,8 +1,10 @@
-$(function(){
-   $('.btn-fix, .btn-fix i').click(function(e){
+var btn_fix_click = function(e){
       e.stopPropagation();
       var t = $(e.target), t = (t.prop("tagName") == 'I' ? t.parent(): t);
       t.toggleClass('btn-success').toggleClass('btn-danger');
       t.siblings('input[type=text]').data((t.hasClass('btn-suffix') ? 'suffix': 'prefix'), (t.hasClass('btn-success') ? true: false));
-   });
+};
+
+$(function(){
+   $('.btn-fix, .btn-fix i').click(btn_fix_click);
 })
